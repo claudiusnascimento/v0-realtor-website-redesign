da50cac1292b7ed61dfd5af128a3a902d5d5bbe7
Claudius Nascimento <claudiusnascimento@gmail.com>
2026-05-25 23:14:19 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    output: 'export',
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: '/v0-realtor-website-redesign',
 }
 
 export default nextConfig
